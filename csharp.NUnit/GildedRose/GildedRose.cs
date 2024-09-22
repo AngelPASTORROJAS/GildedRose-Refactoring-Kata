d11524099e80d3c3669ff5b4305d7087021f82d7
Angel Daniel PASTOR ROJAS <angeldaniel.pastorrojas@gmail.com>
2024-09-22 04:08:12 +0200

    refactor: refoctor on switch case

--- csharp.NUnit/GildedRose/GildedRose.cs
+++ csharp.NUnit/GildedRose/GildedRose.cs
@@ -15,56 +15,52 @@ public class GildedRose
     {
         foreach (Item item in Items)
         {
-            if ((isAgedBrie(item) || isPasses(item)) && item.Quality < 50)
+            switch (item.Name)
             {
-                item.Quality++;
-            }
-            if (item.Quality < 50 && isPasses(item) && item.SellIn < 11)
-            {
-                item.Quality++;
-                if (item.Quality < 50 && item.SellIn < 6)
-                {
-                    item.Quality++;
-                }
-            }
-            if (isNotSulfuras(item))
-            {
-                item.SellIn--;
-                if (!(isAgedBrie(item) || isPasses(item)) && item.Quality > 0)
-                {
-                    item.Quality--;
-                }
-                if (item.SellIn < 0)
-                {
-                    if (isAgedBrie(item) && item.Quality < 50)
+                case "Aged Brie":
+                    if (item.Quality < 50)
                     {
                         item.Quality++;
                     }
-                    if (isPasses(item))
+                    item.SellIn--;
+                    if (item.SellIn < 0 && item.Quality < 50)
+                    {
+                        item.Quality++;
+                    }
+                    break;
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    if (item.Quality < 50)
+                    {
+                        item.Quality++;
+                    }
+                    if (item.Quality < 50 && item.SellIn < 11)
+                    {
+                        item.Quality++;
+                        if (item.Quality < 50 && item.SellIn < 6)
+                        {
+                            item.Quality++;
+                        }
+                    }
+                    item.SellIn--;
+                    if (item.SellIn < 0)
                     {
                         item.Quality = 0;
                     }
-                    if (!(isAgedBrie(item)) && !(isPasses(item)) && item.Quality > 0)
+                    break;
+                case "Sulfuras, Hand of Ragnaros":
+                    break;
+                default:
+                    item.SellIn--;
+                    if (item.Quality > 0)
                     {
                         item.Quality--;
                     }
-                }
+                    if (item.SellIn < 0 && item.Quality > 0)
+                    {
+                        item.Quality--;
+                    }
+                    break;
             }
-        }
-
-        static bool isAgedBrie(Item item)
-        {
-            return item.Name == "Aged Brie";
-        }
-
-        static bool isPasses(Item item)
-        {
-            return item.Name == "Backstage passes to a TAFKAL80ETC concert";
-        }
-
-        static bool isNotSulfuras(Item item)
-        {
-            return item.Name != "Sulfuras, Hand of Ragnaros";
         }
     }
 }
